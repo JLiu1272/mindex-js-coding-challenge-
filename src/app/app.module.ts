4235cca5a785e289bba0c2760a69d35c90f55058
JLiu1272 <jenniferliu1272@g.rit.edu>
2020-11-06 15:13:07 -0500

    Added Delete Direct Report Dialog.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,15 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { DeleteDirectReportDialogComponent } from './delete-direct-report-dialog/delete-direct-report-dialog.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    DeleteDirectReportDialogComponent
   ],
   imports: [
     BrowserModule,
