b608b9f8ba5c671a6b9bd361ae5a4d4c5433fbdf
JLiu1272 <jenniferliu1272@g.rit.edu>
2020-11-18 20:52:17 -0500

    added testing for services.

---
+++ src/app/employee.service.spec.ts
@@ -0,0 +1,99 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { of, from } from 'rxjs';
+
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { flatMap, catchError, map, reduce } from 'rxjs/operators';
+
+describe('EmployeeService', () => {
+    let employeeService: EmployeeService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                EmployeeService,
+                HttpClient,
+                HttpHandler
+            ]
+        });
+
+        employeeService = TestBed.inject(EmployeeService);
+    });
+
+    it('employeeService should be created', () => {
+        expect(employeeService).toBeTruthy();
+    });
+
+    it('get(1) should return information about employeeId 1', () => {
+        const employeeExpectedRes = {
+            id: 1,
+            firstName: 'Brian',
+            lastName: 'McGee',
+            position: 'CEO',
+            directReports: [2, 3],
+            compensation: 0,
+        }
+        let employeeRecRes;
+        spyOn(employeeService, 'get').and.returnValue(of(employeeExpectedRes));
+
+        employeeService.get(1).subscribe(emp => {
+            employeeRecRes = emp;
+        });
+
+        expect(employeeRecRes).toEqual(employeeExpectedRes);
+    })
+
+    it('getAll should return all of the employees', () => {
+        const rawEmployeeData = [
+            {
+                id: 1,
+                firstName: 'Brian',
+                lastName: 'McGee',
+                position: 'CEO',
+                directReports: [2, 3],
+                compensation: 0
+            },
+            {
+                id: 2,
+                firstName: 'Homer',
+                lastName: 'Thompson',
+                position: 'Dev Manager',
+                directReports: [4],
+                compensation: 0
+            },
+            {
+                id: 3,
+                firstName: 'Rock',
+                lastName: 'Strongo',
+                position: 'Lead Tester',
+                directReports: [],
+                compensation: 0
+            },
+            {
+                id: 4,
+                firstName: 'Max',
+                lastName: 'Power',
+                position: 'Junior Software Engineer',
+                directReports: [],
+                compensation: 0
+            }
+        ]
+
+        const employeeExpectedRes = of(rawEmployeeData).pipe(
+            flatMap(emps => from(emps))
+        )
+
+        let employeeRecRes: Employee[];
+        spyOn(employeeService, 'getAll').and.returnValue(employeeExpectedRes)
+
+        employeeService.getAll()
+            .pipe(
+                reduce((emps, e: Employee) => emps.concat(e), []),
+                map(emps => employeeRecRes = emps),
+            ).subscribe();
+
+        expect(employeeRecRes).toEqual(rawEmployeeData);
+    })
+
+});
--- src/app/employee/employee.component.spec.ts
+++ src/app/employee/employee.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { EmployeeComponent } from './employee.component';
 import { EmployeeService } from '../employee.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
+import { Employee } from '../employee';
 
 @Component({ selector: 'app-mat-card', template: '' })
 class CardComponent {
@@ -29,6 +31,8 @@ class CardContentComponent {
 const employeeServiceSpy = jasmine.createSpyObj('EmployeeService', ['getAll', 'get', 'save', 'remove']);
 
 describe('EmployeeComponent', () => {
+  let employeeService: EmployeeService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -44,7 +48,13 @@ describe('EmployeeComponent', () => {
         HttpHandler
       ]
     }).compileComponents();
+
+    employeeService = TestBed.inject(EmployeeService)
   }));
+
+  it('employeeService should be created', () => {
+    expect(employeeService).toBeTruthy();
+  })
 
   it('should create the component', async(() => {
     const fixture = TestBed.createComponent(EmployeeComponent);
@@ -59,8 +69,16 @@ describe('EmployeeComponent', () => {
     expect(comp).toBeTruthy();
   }));
 
-  // Test #1 - getEmploymentDetails gets the desired result 
-  it('getEmploymentDetails returns desired result', async(() => {
 
-  }))
+  // Testing services
+  // it('#get should return value from employeeService', () => {
+  //   const employeeStub = {
+  //     id: 1,
+  //     firstName: 'Brian',
+  //     lastName: 'McGee',
+  //     position: 'CEO',
+  //     directReports: [2, 3],
+  //     compensation: 0,
+  //   }
+
 });
